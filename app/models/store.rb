b6faa05197292e545395cc7b61597b55106be5ef
jdeslangles <julien.deslangles@gmail.com>
2013-12-05 15:07:54 +0000

    fixed store.rb

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,19 @@
 class Store
   include MongoMapper::Document
+  include Tire::Model::Search
+  include Tire::Model::Callback
+  include Geocoder::Model::MongoMapper
 
   key :address, String
-  key :lon_lat, String
+  key :lon_lat, Array
+  timestamps!
+
+  geocoded_by :address, :coordinates => :lon_lat
+  after_validation :geocode #autofetch coords
+
+  mapping do 
+    indexes :address, type: 'string'
+    indexes :lon_lat, type: 'geo_point'
+  end
 
 end
