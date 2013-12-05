7845c5568702f3d0894322cc54610d3b35cbbe21
jdeslangles <julien.deslangles@gmail.com>
2013-12-05 14:55:29 +0000

    fixed product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product
   include MongoMapper::Document
+  include Tire::Model::Search
+  include Tire::Model::Callback
 
   key :name, String
   key :description, String
   key :manufacturer, String
   key :price, Integer
+  timestamps!
 
+  has_many :reviews
 end
