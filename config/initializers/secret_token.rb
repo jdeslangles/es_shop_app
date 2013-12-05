813fcc40eff1df406b3d3fd487e383ef334d61f6
jdeslangles <julien.deslangles@gmail.com>
2013-12-05 14:37:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EsShop::Application.config.secret_token = '1498f076a5ed0f5fd64e4be6d2e7eb3b69a53f1afd363f209cbf95e550a83d39193bd516e7f6c712b4c422f60ea3ca2bbc699f39e5e591cd0ec4001c25521b42'
